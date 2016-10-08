dae3b457be90cc71c5cdf9d3e853cca8874570e9
Gregory Petukhov <lorien@lorien.name>
2016-10-08 17:01:18 +0600

    Refactor client side into single page application

--- web.py
+++ web.py
@@ -1,12 +1,11 @@
-from bottle import route, Bottle, request, response, static_file
-from jinja2 import Environment, FileSystemLoader
+from bottle import route, Bottle, request, response
 from random import SystemRandom
 from baseconv import BaseConverter
 import sys
 import re
-import logging
 from peewee import IntegrityError
 import os
+import json
 
 import settings
 from database import db, Dump
@@ -14,17 +13,8 @@ from database import db, Dump
 GOOD_CHARS = 'abcdefghkmnpqrstwxyz'
 GOOD_DIGITS = '23456789'
 CRYPTO_CHARS = GOOD_CHARS + GOOD_CHARS.upper() + GOOD_DIGITS
-ENV = Environment(loader=FileSystemLoader([
-    os.path.join(settings.BASE_DIR, 'templates_local'),
-    os.path.join(settings.BASE_DIR, 'templates'),
-]))
-CONV = BaseConverter(CRYPTO_CHARS)
-app = Bottle()
-
-
-def render(tpl_name, **kwargs):
-    tpl = ENV.get_template(tpl_name)
-    return tpl.render(**kwargs)
+NUM_SYSTEM_CONVERTOR = BaseConverter(CRYPTO_CHARS)
+APP = Bottle()
 
 
 def create_dump(data, has_password):
@@ -42,58 +32,55 @@ def create_dump(data, has_password):
     raise Exception('Could not generate unique ID for new dump')
 
 
-@app.route('/')
-def home_page():
-    return render('add.html')
+@APP.route('/')
+def page_home():
+    return open(os.path.join(settings.BASE_DIR, 'templates/app.html'))
 
 
-@app.route('/dump/add', ['GET', 'POST'])
-def add_page():
-    if request.method == 'GET':
-        return render('add.html')
+@APP.route('/api/dump', ['POST'])
+def api_dump_post():
+    isbot = request.forms.getunicode('iambot')
+    if isbot:
+        response.status = 403
+        return 'Access denied'
     else:
-        isbot = request.forms.getunicode('iambot')
-        if isbot:
-            response.status = 403
-            return 'Access denied'
-        else:
-            data = request.forms.getunicode('data')
-            has_password = request.forms.getunicode('has_password') == 'yes'
-            if not data:
-                return render('add.html', data_error='Data is empty')
-            dump_id = create_dump(data, has_password)
-            #import pdb; pdb.set_trace()
-            short_id = CONV.encode(dump_id)
-            response.headers['location'] = '/%s' % short_id
-            response.status = 302
-            return response
+        data = request.forms.getunicode('data')
+        has_password = request.forms.getunicode('has_password') == 'yes'
+        if not data:
+            return render('add.html', data_error='Data is empty')
+        dump_id = create_dump(data, has_password)
+        short_id = NUM_SYSTEM_CONVERTOR.encode(dump_id)
+        return json.dumps({'dump_id': short_id})
 
 
-@app.route('/<short_id:re:[a-zA-Z0-9]{1,20}>')
-def dump_page(short_id):
+@APP.route('/api/dump', ['GET'])
+def api_dump_get():
+    short_id = request.query.dump_id
     if re.compile('^[%s]+$' % CRYPTO_CHARS).match(short_id):
-        dump_id = int(CONV.decode(short_id))
+        dump_id = int(NUM_SYSTEM_CONVERTOR.decode(short_id))
         if dump_id <= sys.maxsize:
             try:
                 dump = Dump.get(Dump.id == dump_id)
             except Dump.DoesNotExist:
                 pass
             else:
-                return render('dump.html', data=dump.data,
-                              has_password=dump.has_password)
-    response.status = 404
-    return '<h3 style="color: red">Invalid dump ID</h3>'
+                return json.dumps({
+                    'dump': {
+                        'has_password': bool(dump.has_password),
+                        'data': dump.data,
+                    }
+                })
+    return json.dumps({
+        'error': 'Invalid dump ID'
+    })
 
 
-@app.route('/static/<filepath:path>')
-def server_static(filepath):
-    return static_file(filepath, root='static')
-
-
-@app.route('/help/faq')
-def faq_page():
-    return render('faq.html')
+@APP.route('/<short_id:re:[a-zA-Z0-9]{1,20}>')
+def compatibility_redirect(short_id):
+    response.headers['location'] = '/#%s' % short_id
+    response.status = 302
+    return response
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=9000, debug=True, reloader=True)
+    APP.run(host='localhost', port=9000, debug=True, reloader=True)
